97f94338804364a2c173c8c6a11f7ec153b75bf3
9Bon <tayasriel@gmail.com>
2022-02-27 12:27:35 +0900

    캐시를 전부 다 메모리에 올리게 시도하려 했음, 생각해보니 캐시 리스트만 만들고 필요할때 그 파일을 읽게 만들어야겠음

--- fileIO.py
+++ fileIO.py
@@ -1,3 +1,51 @@
 #cache 디렉토리 안에 보관됨
 #페이지가 이미 조사된, 콘셉트가 이미 조사된, 역링크가 이미 조사된... 맨션이 무언인지 남기는 파일
 #이하 추가 디렉토리로 각 맨션에 해당하는 페이지, 콘셉트, 역링크가 저장됨
+#crawling.py 에서 사용됨
+
+import os
+
+class FileIO():
+	def __init__(self):
+
+                self.dictStringOfMentionAndRequests = dict()
+                self.dictSetOfMentionAndgetLinks = dict()
+                self.dictSetOfMentionAndgetBackLinks = dict()
+                self.dictSetOfMentionAndgetContents = dict()
+
+                self.FILEREQUESTS = './cache/MentionAndRequests/'
+                self.FILELINKS = './cache/MentionAndgetLinks/'
+                self.FILEBACKLINKS = './cache/MentionAndgetBackLinks/'
+                self.FILECONTENTS = './cache/MentionAndgetContents/'
+        #load--------------------------------------------------------------
+        #초기 불러오기 서브 루틴
+        def filesToDict(self, locale, dic):
+                filenames = os.listdir(locale)
+                for filename in filenames:
+                        f = open(locale + filename)
+                        lines = f.readlines()
+                        f.close
+                        size = len(lines) - 1
+                        res = set()
+                        for i in range(size):
+                                res.add(lines[i])
+                        dic[filename] = res
+                return
+        #초기 불러오기 메인 루틴, 모든 dict를 채운다.
+        def loadDicts(self):
+                self.filesToDict(self.FILELINKS, self.dictSetOfMentionAndgetLinks)
+                self.filesToDict(self.FILEBACKLINKS, self.dictSetOfMentionAndgetBackLinks)
+                self.filesToDict(self.FILECONTENTS, self.dictSetOfMentionAndgetContents)
+
+                filenames = os.listdir(self.FILEREQUESTS)
+                for filename in filenames:
+                        f = open(self.FILEREQUESTS + filename)
+                        self.dictStringOfMentionAndRequests[filename] = f.read()
+                        f.close
+                return
+
+        #cheack&return-----------------------------------------------------------
+
+        #save&add-----------------------------------------------------------
+        def setToFile_putDict(self, ch, inp):
+
