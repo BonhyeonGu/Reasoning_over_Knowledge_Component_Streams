9ac3504d0903bdb1269ed3dd45d414744a94c993
9Bon <tayasriel@gmail.com>
2022-02-28 23:12:25 +0900

    리퀘스트, 링크스 '보조기억장치에 캐싱하기' 추가 완료

--- crawling.py
+++ crawling.py
@@ -4,34 +4,57 @@ from threading import Thread
 from queue import Queue
 
 from util import Util
+from fileIO import FileIO
 
 from multiprocessing import Process, freeze_support, Pool
 from functools import partial
 import time
 import datetime
 
+
+
 class Crawling:
-    #입력 : 컨셉, 리턴 : 집합
+    def __init__(self):
+        self.fc = FileIO()
+        self.fc.loadSets()
+
+    #원래는 urlToSoup함수에 캐시 저장/불러오기를 만들려고 했으나, 해당 함수는 일반 위키페이지를 포함한 모든 페이지에 사용되기에(ex역링크)
+    #올바르지 않았다. 따라서 일반 위키 페이지를 검색하는 함수를 전용으로 만드는게 괜찮겠다.
+    def urlToSoupOnlyNormal(self, keyword):
+        cheack = self.fc.getCache(0, keyword)
+        if cheack != -1:
+            soup = BeautifulSoup(cheack, 'lxml')
+        else:
+            req = requests.get('https://en.wikipedia.org/wiki/' + keyword)
+            self.fc.setToFile(0, keyword, req.text)
+            soup = BeautifulSoup(req.text, 'lxml')
+        return soup
+
     def urlToSoup(self, url):
         req = requests.get(url)
         soup = BeautifulSoup(req.text, 'lxml')
         return soup
 
-    #해당 멘션 페이지의 컨셉들을 찾는다. 존재하지 않는 멘션이라는 것도 여기서 필터링 된다. 존재하지 않는 멘션이면 return None
+    #해당 멘션 페이지의 앵커들을 찾는다. !!!존재하지 않는 멘션이라는 것도 여기서 필터링 된다. 존재하지 않는 멘션이면 return None!!! <---이거 뺄 예정
     def getLinks(self, mention):
-        ret = set()
-        soup = self.urlToSoup('https://en.wikipedia.org/wiki/' + mention)
-        tag = soup.select_one('#noarticletext > tbody > tr > td > b')
-        #존재하지 않는 검색어라는 메세지 발생시 None을 리턴
-        if tag is not None:
-            return None
-        tag = soup.select_one('#mw-content-text')
-        tags = tag.select("a[href^='/wiki/']")
-        for tag in tags:
-            #중복되지 않았다면, (명확성 안내 링크 삭제, ?자기이름 링크 삭제?, 파일이 아니면, 세미콜론 들어가는거만 빼주면 될거같다.)
-            if (tag['href'] not in ret)and(':' not in tag['href']):
-                ret.add(tag['href'].split('/')[2])#/wiki/~
-        return ret
+        cheack = self.fc.getCache(1, mention)
+        if cheack != -1:
+            return cheack
+        else:
+            ret = set()
+            soup = self.urlToSoupOnlyNormal(mention)
+            tag = soup.select_one('#noarticletext > tbody > tr > td > b')
+            #존재하지 않는 검색어라는 메세지 발생시 None을 리턴
+            if tag is not None:
+                return None
+            tag = soup.select_one('#mw-content-text')
+            tags = tag.select("a[href^='/wiki/']")
+            for tag in tags:
+                #중복되지 않았다면, (명확성 안내 링크 삭제, ?자기이름 링크 삭제?, 파일이 아니면, 세미콜론 들어가는거만 빼주면 될거같다.)
+                if (tag['href'] not in ret)and(':' not in tag['href']):
+                    ret.add(tag['href'].split('/')[2])#/wiki/~
+            self.fc.setToFile(1, mention, ret)
+            return ret
    
     #해당 단어로 향하는 하이퍼링크가 있는 페이지를 찾는다.Lc 이것을 위키에서는 백링크라 부르더라
     def getBacklinks(self, p):
@@ -55,19 +78,10 @@ class Crawling:
         que.put(ret)
         return
 
-    #getAnkers와 흡사하지만 추가 조건이 붙는다.
+    #getLinks와 흡사하지만 추가 조건이 붙는다.
     #컨셉은 해당 조건으로 탈락된다. 컨셉으로 가는 하이퍼링크가 있는 페이지들 중에 맨션으로 가는 링크가 있는 페이지들을 새알린 뒤 2개 미만이면 탈락된다.
-    def getConceptsSingle(self, mention):
-        ret = set()
-        pagesInMention = self.getBacklinks(mention)
-        concepts = self.getLinks(mention)#컨셉 후보
-        for concept in concepts:
-            pagesInConcept = self.getBacklinks(concept)
-            if len(pagesInMention & pagesInConcept) >= 2:
-                ret.add(concept)
-        return ret
-
-    def getConceptsMultiThread(self, mention):
+    #해당 함수는 멀티 쓰레딩이다.
+    def getConcepts(self, mention):
         ret = set()
         TC = 12
         pagesInMention = self.getBacklinks(mention)
@@ -105,7 +119,7 @@ class Crawling:
 if __name__ == '__main__':
     c = Crawling()
     timeStart = time.time()
-    a=c.getConceptsMultiThread('apple')
+    a=c.getLinks('cat')
     print(a)
     print(len(a))
     timeEnd = time.time()
--- fileIO.py
+++ fileIO.py
@@ -6,46 +6,72 @@
 import os
 
 class FileIO():
-	def __init__(self):
+    def __init__(self):
+        #기회가 되면 알아서 불러오게 해볼까
+        self.okListRequests = ['./Cache/MentionAndRequests/', set()]
+        self.okListLinks = ['./Cache/MentionAndgetLinks/', set()]
+        self.okListBacklinks = ['./Cache/MentionAndgetBackLinks/', set()]
+        self.okListContents = ['./Cache/MentionAndgetContents/', set()]
 
-                self.dictStringOfMentionAndRequests = dict()
-                self.dictSetOfMentionAndgetLinks = dict()
-                self.dictSetOfMentionAndgetBackLinks = dict()
-                self.dictSetOfMentionAndgetContents = dict()
+    #load--------------------------------------------------------------
+    #sub
+    def filesToSet(self, sett):
+        filenames = os.listdir(sett[0])
+        for filename in filenames:
+            sett[1].add(filename)
+        return
+    #main
+    def loadSets(self):
+        self.filesToSet(self.okListRequests)
+        self.filesToSet(self.okListLinks)
+        self.filesToSet(self.okListBacklinks)
+        self.filesToSet(self.okListContents)
+        return
 
-                self.FILEREQUESTS = './cache/MentionAndRequests/'
-                self.FILELINKS = './cache/MentionAndgetLinks/'
-                self.FILEBACKLINKS = './cache/MentionAndgetBackLinks/'
-                self.FILECONTENTS = './cache/MentionAndgetContents/'
-        #load--------------------------------------------------------------
-        #초기 불러오기 서브 루틴
-        def filesToDict(self, locale, dic):
-                filenames = os.listdir(locale)
-                for filename in filenames:
-                        f = open(locale + filename)
-                        lines = f.readlines()
-                        f.close
-                        size = len(lines) - 1
-                        res = set()
-                        for i in range(size):
-                                res.add(lines[i])
-                        dic[filename] = res
-                return
-        #초기 불러오기 메인 루틴, 모든 dict를 채운다.
-        def loadDicts(self):
-                self.filesToDict(self.FILELINKS, self.dictSetOfMentionAndgetLinks)
-                self.filesToDict(self.FILEBACKLINKS, self.dictSetOfMentionAndgetBackLinks)
-                self.filesToDict(self.FILECONTENTS, self.dictSetOfMentionAndgetContents)
-
-                filenames = os.listdir(self.FILEREQUESTS)
-                for filename in filenames:
-                        f = open(self.FILEREQUESTS + filename)
-                        self.dictStringOfMentionAndRequests[filename] = f.read()
-                        f.close
-                return
-
-        #cheack&return-----------------------------------------------------------
-
-        #save&add-----------------------------------------------------------
-        def setToFile_putDict(self, ch, inp):
-
+    #cheack&return-----------------------------------------------------------
+    #아래 두개는 더 겹치는게 없게 코딩할 수 있는데, 그냥 보기 편하게 만듬
+    #순서대로 리퀘스트, 링크, 백링크, 콘텐츠이며 리퀘스트만 온리 문자열이기 때문에 다르게 처리한다. 리턴이 정수거나 문자열이거나 행렬이다.
+    def getCache(self ,ch, filename):
+        if ch == 0:
+            if not filename in self.okListRequests[1]:
+                return -1
+            else:
+                f = open(self.okListRequests[0] + filename, 'r')
+                return f.read()
+        elif ch == 1:
+            sett = self.okListLinks
+        elif ch == 2:
+            sett = self.okListBacklinks
+        elif ch == 3:
+            sett = self.okListContents
+        if not filename in sett[1]:
+            return -1
+        else:
+            ret = set()
+            f = open(sett[0] + filename, 'r')
+            lines = f.readlines()
+            size = len(lines) - 1
+            for i in range(size):
+                ret.add(lines[i])
+            return ret
+        
+    #save&add-----------------------------------------------------------
+    def setToFile(self, ch, filename, inp):
+        if ch == 0:
+            self.okListRequests[1].add(filename)
+            f = open(self.okListRequests[0] + filename, 'w', encoding='UTF-8')
+            f.write(inp)
+            f.close()
+            return
+        elif ch == 1:
+            sett = self.okListLinks
+        elif ch == 2:
+            sett = self.okListBacklinks
+        elif ch == 3:
+            sett = self.okListContents
+        sett[1].add(filename)
+        f = open(sett[0] + filename, 'w', encoding='UTF-8')
+        for s in inp:
+            f.write(s + '\n')
+        f.close()
+        return
