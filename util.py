6689dd1b63bd572c6ac43932e886a1911dfd2080
9Bon <tayasriel@gmail.com>
2022-03-09 18:43:03 +0900

    스플릿 리스트에서 0으로 나누는 문제 해결

--- util.py
+++ util.py
@@ -14,6 +14,9 @@ class Util:
         return ret
 
     def splitList(lis, splitCount):
+        if len(lis) <= splitCount:
+            return [lis]
+
         ret = []
         size = len(lis) // splitCount
         tmp = []
@@ -21,7 +24,7 @@ class Util:
         for e in lis:
             tmp.append(e)
             idx += 1
-            if idx % size == 0:
+            if idx % size == 0:#0
                 ret.append(tmp)
                 tmp = []
         ret.append(tmp)
