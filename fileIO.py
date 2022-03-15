a8f7e7e205c5312fa4cccc6ef410ade758b1ddd2
9Bon <tayasriel@gmail.com>
2022-03-15 21:43:23 +0900

    리퀘스트 캐싱 취소

--- fileIO.py
+++ fileIO.py
@@ -9,10 +9,11 @@ import os
 class FileIO():
     def __init__(self):
         #기회가 되면 알아서 불러오게 해볼까
-        self.okListRequests = ['./Cache/MentionAndRequests/', set()]
+        #self.okListRequests = ['./Cache/MentionAndRequests/', set()]
         self.okListLinks = ['./Cache/MentionAndgetLinks/', set()]
         self.okListTexts = ['./Cache/MentionAndgetTexts/', set()]
         self.okListBacklinks = ['./Cache/MentionAndgetBackLinks/', set()]
+        self.okListEntrophys = ['./Cache/MentionAndgetEntrophys/', set()]
         self.okListContents = ['./Cache/MentionAndgetContents/', set()]
 
         self.banList = set(['\\', ':', '"', '<', '>', '|'])
@@ -42,10 +43,11 @@ class FileIO():
         return
     #main
     def loadSets(self):
-        self.filesToSet(self.okListRequests)
+        #self.filesToSet(self.okListRequests)
         self.filesToSet(self.okListLinks)
         self.filesToSet(self.okListTexts)
         self.filesToSet(self.okListBacklinks)
+        self.filesToSet(self.okListEntrophys)
         self.filesToSet(self.okListContents)
         return
 
@@ -53,20 +55,21 @@ class FileIO():
     #아래 두개는 더 겹치는게 없게 코딩할 수 있는데, 그냥 보기 편하게 만듬
     #순서대로 리퀘스트, 앵커링크, 앵커텍스트, 백링크, 콘텐츠이며 리퀘스트만 온리 문자열이기 때문에 다르게 처리한다. 리턴이 정수거나 문자열이거나 행렬이다.
     def getCache(self ,ch, filename):
-
-        if ch == 0:
-            if not filename in self.okListRequests[1]:
-                return -1
-            else:
-                f = open(self.okListRequests[0] + self.nameEncode(filename), 'r', encoding='UTF-8')
-                return f.read()
-        elif ch == 1:
+        # if ch == 0:
+        #     if not filename in self.okListRequests[1]:
+        #         return -1
+        #     else:
+        #         f = open(self.okListRequests[0] + self.nameEncode(filename), 'r', encoding='UTF-8')
+        #         return f.read()
+        if ch == 1:
             sett = self.okListLinks
         elif ch == 2:
             sett = self.okListTexts
         elif ch == 3:
             sett = self.okListBacklinks
         elif ch == 4:
+            sett = self.okListEntrophys
+        elif ch == 5:
             sett = self.okListContents
         if not filename in sett[1]:
             return -1
@@ -85,25 +88,26 @@ class FileIO():
         for c in filename:
             if c in self.banList:
                 return
-
         realFilename = self.nameEncode(filename)
         #파일길이 확인
         if len(realFilename) > 260:
             return
 
-        if ch == 0:
-            self.okListRequests[1].add(filename)
-            f = open(self.okListRequests[0] + realFilename, 'w', encoding='UTF-8')
-            f.write(inp)
-            f.close()
-            return
-        elif ch == 1:
+        # if ch == 0:
+        #     self.okListRequests[1].add(filename)
+        #     f = open(self.okListRequests[0] + realFilename, 'w', encoding='UTF-8')
+        #     f.write(inp)
+        #     f.close()
+        #     return
+        if ch == 1:
             sett = self.okListLinks
         elif ch == 2:
             sett = self.okListTexts
         elif ch == 3:
             sett = self.okListBacklinks
         elif ch == 4:
+            sett = self.okListEntrophys
+        elif ch == 5:
             sett = self.okListContents
         sett[1].add(filename)
         f = open(sett[0] + realFilename, 'w', encoding='UTF-8')
