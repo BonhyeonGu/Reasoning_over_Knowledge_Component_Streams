656597bf88617415546519eb0baa394d2275d589
9Bon <tayasriel@gmail.com>
2022-03-20 21:34:23 +0900

    콘셉트와 엔트로피 캐싱추가, 오류를 -1로 통일하고 try except를 추가함

--- crawling.py
+++ crawling.py
@@ -10,34 +10,27 @@ from fileIO import FileIO
 
 from multiprocessing import Process, freeze_support, Pool
 from functools import partial
-import time
-import datetime
 
 class Crawling:
     def __init__(self):
         self.fc = FileIO()
+        #나중에는 엥커와 백링크, 엔트로피와 콘셉트로 각각 따로 메모리에 셋을 할까
         self.fc.loadSets()
 
     #원래는 urlToSoup함수에 캐시 저장/불러오기를 만들려고 했으나, 해당 함수는 일반 위키페이지를 포함한 모든 페이지에 사용되기에(ex역링크)
     #올바르지 않았다. 따라서 일반 위키 페이지를 검색하는 함수를 전용으로 만드는게 괜찮겠다.
     def urlToSoupOnlyNormal(self, keyword):
-        try:
-            req = requests.get('https://en.wikipedia.org/wiki/' + keyword)
-        except Exception as e:
-            time.sleep(uniform(0.5, 1.0))
+        while(True):
             try:
                 req = requests.get('https://en.wikipedia.org/wiki/' + keyword)
+                break
             except Exception as e:
-                time.sleep(uniform(1.0, 1.5))
-                try:
-                    req = requests.get('https://en.wikipedia.org/wiki/' + keyword)
-                except Exception as e:
-                    print("오류!!! urlToSoupOnlyNormal 커넥션 실패")
+                time.sleep(uniform(0.5, 1.0))                
         soup = BeautifulSoup(req.text, 'lxml')
-        tag = soup.select_one('#mw-content-text')
+        # tag = soup.select_one('#mw-content-text')
         # if tag == None:
         #     time.sleep(uniform(4.5, 5.5))
-        #     self.urlToSoupOnlyNormal(keyword)
+        #     return self.urlToSoupOnlyNormal(keyword)
         #self.fc.setToFile(0, keyword, req.text)
         return soup
 
@@ -58,20 +51,27 @@ class Crawling:
         soup = BeautifulSoup(req.text, 'lxml')
         return soup
 
-    #해당 멘션 페이지의 앵커링크들을 찾는다. !!!존재하지 않는 멘션이라는 것도 여기서 필터링 된다. 존재하지 않는 멘션이면 return None!!! <---이거 뺄 예정
+    #해당 멘션 페이지의 앵커링크들을 찾는다. !!!존재하지 않는 멘션이라는 것도 여기서 필터링 된다. 존재하지 않는 멘션이면 return -1 <---이거 있어야겠네
     def getLinks(self, mention):
         cheack = self.fc.getCache(1, mention)
         if cheack != -1:
             return cheack
         else:
+            soup = self.urlToSoupOnlyNormal(mention)
+            #---------------------------------------------------------------
+            #존재하지 않는 검색어라는 메세지 발생시 None을 리턴
+            tag = soup.select_one('#noarticletext > tbody > tr > td > b')
+            if tag is not None:
+                return -1
+            #---------------------------------------------------------------
+            tag = soup.select_one('#mw-content-text')
+            #---------------------------------------------------------------
+            #다른 기타 오류
+            if tag == None:
+                return -1
+            #---------------------------------------------------------------
             ret_links = set()
             ret_texts = set()
-            soup = self.urlToSoupOnlyNormal(mention)
-            tag = soup.select_one('#noarticletext > tbody > tr > td > b')
-            #존재하지 않는 검색어라는 메세지 발생시 None을 리턴
-            if tag is not None:
-                return None
-            tag = soup.select_one('#mw-content-text')
             tags = tag.select("a[href^='/wiki/']")
             for tag in tags:
                 #중복되지 않았다면, (명확성 안내 링크 삭제, ?자기이름 링크 삭제?, 파일이 아니면, 세미콜론 들어가는거만 빼주면 될거같다.)
@@ -88,14 +88,21 @@ class Crawling:
         if cheack != -1:
             return cheack
         else:
+            soup = self.urlToSoupOnlyNormal(mention)
+            #---------------------------------------------------------------
+            #존재하지 않는 검색어라는 메세지 발생시 None을 리턴
+            tag = soup.select_one('#noarticletext > tbody > tr > td > b')
+            if tag is not None:
+                return -1
+            #---------------------------------------------------------------
+            tag = soup.select_one('#mw-content-text')
+            #---------------------------------------------------------------
+            #다른 기타 오류 삭제된 페이지 포함
+            if tag == None:
+                return -1
+            #---------------------------------------------------------------
             ret_links = set()
             ret_texts = set()
-            soup = self.urlToSoupOnlyNormal(mention)
-            tag = soup.select_one('#noarticletext > tbody > tr > td > b')
-            #존재하지 않는 검색어라는 메세지 발생시 None을 리턴
-            if tag is not None:
-                return None
-            tag = soup.select_one('#mw-content-text')
             tags = tag.select("a[href^='/wiki/']")
             for tag in tags:
                 #중복되지 않았다면, (명확성 안내 링크 삭제, ?자기이름 링크 삭제?, 파일이 아니면, 세미콜론 들어가는거만 빼주면 될거같다.)
@@ -123,16 +130,6 @@ class Crawling:
             self.fc.setToFile(3, p, ret)
             return ret
 
-    def calcEntrophy(self, allBacklinksNum, asAnchortextNum):#mention A 에 대한 entrophy를 구한다
-        length = len(allBacklinksNum)#길이는 같은걸로 간주한다
-        sum=0
-        for i in range(length):
-            if(asAnchortextNum[i] == 0 or allBacklinksNum[i] == 0):#둘 중 하나라도 0이면 넘김
-                continue
-            temp = asAnchortextNum[i]/allBacklinksNum[i]
-            sum -= temp * math.log10(temp)
-        return sum
-
     #PR0를 구하는 공식에서 분모로 사용될 내용, 정수 리턴
     def getPR0den(self, u):
         url = "https://en.wikipedia.org/w/index.php?title=Special:Search&limit=20&offset=0&ns0=1&search=" + u
@@ -142,14 +139,3 @@ class Crawling:
             return 0
         return int(tag.text.replace(',', ''))
 
-if __name__ == '__main__':
-    c = Crawling()
-    timeStart = time.time()
-    a=c.getLinks('P2pnet')
-    print(a)
-    print(len(a))
-    timeEnd = time.time()
-    sec = timeEnd - timeStart
-    result_list = str(datetime.timedelta(seconds=sec))
-    print(result_list)
-    input()
