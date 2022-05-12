b5319d8463f07bd9163e41c420ca3f9193082da4
9Bon <tayasriel@gmail.com>
2022-05-12 18:40:01 +0900

    계획에 있던대로 유튜브 자막, 스필릿을 통하게 만듬

--- wikificationTest.py
+++ wikificationTest.py
@@ -1,25 +1,55 @@
 from MC_Graph import Graph
+import queue
 import string
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
+from youtube_transcript_api import YouTubeTranscriptApi
+
 class WikificationTest:
-    def __init__(self, text):
-        self.inputForward = text
-        self.preProcess()
-        g = Graph(self.inputForward)
-        result = g.getAnnotation(5)
-        print("\n")
-        for i in range(len(result)):
-            print("node: "+result[i].name)
-            print("PR: %lf"%(result[i].PR[g.IDX]))
-            print("")       
+    def __init__(self, url):
+        sett:queue.Queue = self.urlToSplitQueue(300.0, url)
+        while sett.qsize() != 0:
+            subInSec = sett.get()
+            #print(subInSec)
+            g = Graph(self.preProcess(subInSec))
+            result = g.getAnnotation(5)
+            print("\n")
+            for i in range(len(result)):
+                print("node: "+result[i].name)
+                print("PR: %lf"%(result[i].PR[g.IDX]))
+                print("")       
+
+    def urlToSplitQueue(self, splitSec, url:str):
+        ret = queue.Queue()
+
+        tmp = url.split('?v=')[1]
+        ytid = ''
+        for char in tmp:
+            if char == '&':
+                break
+            ytid += char
         
-    def preProcess(self):
+        nowSec = splitSec
+        srt:list = YouTubeTranscriptApi.get_transcript(ytid, languages=['en'])
+        sentense = ''
+        for st in srt:
+            if st['start'] >= nowSec:
+                ret.put(sentense)
+                sentense = ''
+                nowSec += splitSec
+                if frontSt['start'] + frontSt['duration'] >= nowSec:
+                    sentense += frontSt['text'] + '\n'
+            sentense += st['text'] + '\n'
+            frontSt = st
+        ret.put(sentense)
+        return ret
+            
+    def preProcess(self, inp):
         stopWords = set(stopwords.words('english'))
         #stopWords.update(('',''))
         
-        tokens = word_tokenize(self.inputForward)
+        tokens = word_tokenize(inp)
         # convert to lower case
         #tokens = [w.lower() for w in tokens]
         # remove punctuation from each word
@@ -30,10 +60,10 @@ class WikificationTest:
         words = [word for word in stripped if word.isalpha()]
         
         # filter out stop words
-        self.inputForward = [w for w in words if not w in stopWords]
-        print(self.inputForward)
-        return self.inputForward
+        out = [w for w in words if not w in stopWords]
+        #print(out)
+        return out
 
 with open('./inp.txt', 'rt', encoding='utf-8') as f:
     text = f.read()
-WikificationTest(text)
+WikificationTest('https://www.youtube.com/watch?v=49g2M0Yv4DU')
